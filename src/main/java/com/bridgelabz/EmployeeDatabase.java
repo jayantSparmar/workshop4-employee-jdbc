25f99223413b8208c74e2587d98b17285d083162
jayant <jayantparmar420@gmail.com>
2022-04-23 22:27:54 +0530

    Added : the database connection

--- src/main/java/com/bridgelabz/EmployeeDatabase.java
+++ src/main/java/com/bridgelabz/EmployeeDatabase.java
@@ -1,8 +1,59 @@
 package com.bridgelabz;
 
+import java.sql.*;
+
 public class EmployeeDatabase {
+    public  static Connection getConnection() {
+        Connection conn = null;
+        try {
+            String JDBC_URL = "jdbc:mysql://localhost:3306/database_employee?useSSL=false";
+            String userName = "root";
+            String password = "0177ce151030";
+            Connection connection;
+            System.out.println("Connecting to database: " + JDBC_URL);
+            connection = DriverManager.getConnection(JDBC_URL, userName, password);
+            System.out.println("Connection is successful! " + connection);
+            return connection;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+
+    // Insert data
+    public  static  void insert() {
+        String sql="insert into employee(id,name,date)"+
+                "values('7','Jayant','2022-07-15')";
+
+        Connection conn =getConnection();
+        try {
+            Statement st=conn.createStatement();
+            int count =st.executeUpdate(sql);
+            System.out.println( "Insert "+count+" records!" );
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    // delete data
+    public  static  void delete() {
+        String sql="delete from employee where id=3";
+        Connection conn =getConnection();
+        try {
+            Statement st=conn.createStatement();
+            int count =st.executeUpdate(sql);
+            System.out.println( "Deleted "+count+" records!" );
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
         System.out.println("welcome to employee-jdbc program");
+        EmployeeDatabase jdbc = new EmployeeDatabase();
+        jdbc.insert();
+        jdbc.delete();
     }
 }
